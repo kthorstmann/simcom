59f0380573921361396bbcc469dfde90a8da4ce9
Kai Horstmann <kaitobias@icloud.com>
2017-09-11 13:43:36 +0200

    changed default in functions and added examples

--- R/simcom_functions.R
+++ R/simcom_functions.R
@@ -1,110 +1,7 @@
 
 
 
-# dependencies -------------------------------------------------------
-
-
-
-
 # functions ----------------------------------------------------------
-
-#' Plot Stacked Areas
-#'
-#' @param data The data frame that needs to be plotted. Should only contain the variables that should appear in the plot.
-#' @param poly.border.col The color of the border of the stacked areas.
-#' @param poly.fill.col The fill of the stacked areas. The areas are colored from top to bottom (i.e. the highest one first).
-#' @param letters The letters that should be placed in the areas. If NULL, the names of the data is used.
-#' @param letter.col The colors of the letters that are plotted.
-#' @param ... Further arguments passed on to the plot function plot(...)
-#'
-#' @return Plots areas that are scaled to 100%.
-#' @export
-#'
-#' @examples
-#' ## Not defined yet
-plot_polygons <- function(data,
-                          poly.border.col = "grey10",
-                          poly.fill.col = c("white", "white",
-                                            "white", "white", "#F8CBAE",
-                                            "#FDF2CA", "#9DC3E6"),
-                          letters = NULL,
-                          letter.col = "black",
-                          ...){
-
-  is.num     <-  apply(data, 2, is.numeric)
-  nonNumeric <-  names(data)[!is.num]
-  if (any(!is.num)) {
-    stop(paste("The following items/columns are non-numeric. \n ",
-               nonNumeric, collapse=" "), call.=FALSE)
-  }
-
-  xunits <- (nrow(data)) # determine the number of units, because we want to plot from 0 to n.
-  rownames(data) <- 1:xunits
-  # get percentages:
-  total <- rowSums(data, na.rm=TRUE)
-  perc.dat <- as.data.frame(apply(data, 2, function(x) x/total*100))
-  t.perc.dat <- t(perc.dat)
-  # make a vector over which we apply the function that computes the boundaries for the polygons
-  len <- 2:ncol(data)
-  # apply the function in order to get the borders of the polygons
-  res <- rev(lapply(len, function(x) colSums(t.perc.dat[1:x,], na.rm=TRUE)))
-  # add the last polygon to list
-  res[[length(res)+1]] <- t.perc.dat[1,]
-  # compute the lower border ofthe last polygon, i. e. 0, repeated as many times as data has rows
-  lower <- rep(0, xunits) # +1
-
-  # plot the polygons, define the function
-  plot.poly <- function(x){
-    xcoords <-  c(xunits:1, 1:xunits)
-    ycoords <-  c(lower, res[[x]])
-    polygon(xcoords, ycoords, col = poly.fill.col[x], border = poly.border.col)
-  }
-
-  ## plot an empty area
-  plot(NA, ylim=c(1, 110), xlim = c(0, xunits), type="n",
-       yaxt="n", xaxt="n", bty="n", ylab="", xlab="", ...)
-
-
-  # plot the polygons
-  invisible(lapply(1:length(res), function(x) plot.poly(x) ))
-
-  if (ncol(data) != length(poly.fill.col)) {
-    warning("Only ", length(poly.fill.col), " colors were specified, but ", ncol(data), " areas were plotted. Colors were recycled. Please check.")
-  }
-
-  # add letters, default is to names of data
-  if (is.null(letters) ) { letters <- rev(colnames(data)) }
-
-  # add to the result list with the borders another lower border, i. e. the zeros with names
-  names(lower) <- 0:(length(lower)-1)
-  res[[length(res)+1]] <- lower
-  # apply the function over the borders, computing the borders and plotting the polygon
-  invisible(
-    lapply(1:(length(res)-1), function(x){
-      y = x + 1
-      # make two empty lists
-      list_low <- list()
-      list_up <- list()
-      # reduce the plotting area left and right
-      list_low[[x]] <- res[[x]][ceiling(length(res[[x]])*.08):floor(length(res[[x]])*.90)]
-      list_up[[x]] <- res[[y]][ceiling(length(res[[y]])*.08):floor(length(res[[y]])*.90)]
-
-      if ( nrow(data) >= 12) {
-        # get the position of the letters, which is where the maximum of the difference between the borders is. if there is more than one maximum, take only the first
-        posx = names(which(list_low[[x]]-list_up[[x]] == max(list_low[[x]]-list_up[[x]])))[1]
-        # compute the mean difference, needs to be in the tighter/reduced lists
-        posy = mean(c(list_low[[x]][posx], list_up[[x]][posx]))
-        text(as.numeric(posx), posy, letters[x], col = letter.col)
-      } else {
-        posx <- (nrow(data)-1)
-        # compute the mean, not in the tight lists (but in 'res', to plot the letter to the right hand side)
-        posy = mean(c(res[[x]][posx], res[[y]][posx]))
-        text(posx-posx*0.03, posy, letters[x], col = letter.col)
-      }
-    })
-  )
-}
-
 
 
 # simulate data function ---------------------------------------------
@@ -132,7 +29,7 @@ plot_polygons <- function(data,
 #' @export
 #'
 #' @examples
-#' ## not defined yet
+#' data <- sim_data()
 sim_data <- function(n_pop = 1000,
                      b_s = 0.26,
                      b_e = 0.19,
@@ -170,7 +67,7 @@ sim_data <- function(n_pop = 1000,
   n_var <- ncol(Sigma)
   means_all = 0
   mu <- rep(means_all, n_var)
-  rawvars <- mvrnorm(n=n_pop, mu=mu, Sigma=Sigma)
+  rawvars <- MASS::mvrnorm(n=n_pop, mu=mu, Sigma=Sigma)
   colnames(rawvars) <- c("B", "S", "P", "E")
   raw_data <- data.frame(rawvars)
 
@@ -189,9 +86,9 @@ sim_data <- function(n_pop = 1000,
     bsr <- cor_bs_l1
     l1_bs_matrix <- matrix(c(l1_var, bsr,
                              bsr, l1_var), 2, 2, byrow=TRUE)
-    l1_bs_raw <- mvrnorm(n = l1_bs_population,
-                         mu = c(mu_S_person, mu_B_person),
-                         Sigma = l1_bs_matrix)
+    l1_bs_raw <- MASS::mvrnorm(n = l1_bs_population,
+                               mu = c(mu_S_person, mu_B_person),
+                               Sigma = l1_bs_matrix)
 
     esm_occasions <- sample(1:l1_bs_population, l1_bs_sample, replace = FALSE)
     sample_occasions <- data.frame(l1_bs_raw[esm_occasions,])
@@ -199,14 +96,15 @@ sim_data <- function(n_pop = 1000,
     sample_occasions
   }
 
-  level1_states <- map2(.x = S,
-                        .y = B,
-                        .f = function(x, y) {
-                          simulate_l1_bs(x, y,
-                                         cor_bs_l1 = cor_bs_l1,
-                                         l1_bs_sample = l1_bs_sample)
-                          }
-                        )
+  level1_states <- purrr::map2(.x = S,
+                               .y = B,
+                               .f = function(x, y) {
+                                 simulate_l1_bs(x, y,
+                                                cor_bs_l1 = cor_bs_l1,
+                                                l1_bs_sample = l1_bs_sample)
+                               }
+                               )
+
 
   # helper function to add the id
   cidd <- function(d, i){
@@ -216,12 +114,13 @@ sim_data <- function(n_pop = 1000,
   }
 
   ## add id-variables on level-1
-  level_1_id_states <- map2(.x = level1_states,
-                            .y = id,
-                            .f = function(x, y) {
-                              cidd(x, y)
-                              }
-                            )
+  level_1_id_states <- purrr::map2(.x = level1_states,
+                                   .y = id,
+                                   .f = function(x, y) {
+                                     cidd(x, y)
+                                   }
+                                   )
+
 
   # bind data frames from list into one data frame
   l1_states_data <- do.call("rbind", level_1_id_states)
@@ -230,45 +129,8 @@ sim_data <- function(n_pop = 1000,
   simulated_data <- dplyr::inner_join(raw_data, l1_states_data, by = "id")
   # return data
   return(simulated_data)
+
 }
-
-
-
-# plot components ----------------------------------------------------
-
-
-#' Plot results of components analysis
-#'
-#' @param params_df The results of the function, which is a list with to entries.
-#' @param ... Further arguments passed on to plot_polygons
-#'
-#' @return
-#' Returns a plot with the components
-#' @export
-#'
-#' @examples
-#' ## note defined yet
-plot_com <- function(params_df, ...){
-  params <- params_df[[1]]
-  rsq <- params_df[[2]]
-
-  # par (mar = c(5.1, 4.1, 4.1, 0))
-  plot_polygons(params, ...)
-
-  # plot addional explained variance in the plot
-  rsq_plot <- rsq*10+100
-  lines(rsq_plot, col = "black", lwd = 2, lty = 3)
-  min <- round(rsq[1], 2)
-  max <- round(rsq[length(rsq_plot)], 2)
-  text(x = 1, y = 10 + 100, labels=min)
-  text(x = length(rsq_plot), y = 10 + 100, labels=max)
-
-  axis(2, at = c(0, 20, 40, 60, 80, 100), labels = c(0, 20, 40, 60, 80, 100))
-  # add explained variance
-  axis(4, at=c(100, 105, 110), labels = c(0, 50, 100))
-}
-
-
 
 
 # components analysis  -----------------------------------------------
@@ -289,7 +151,9 @@ plot_com <- function(params_df, ...){
 #' @export
 #'
 #' @examples
-#' ## note defined yet
+#' simulated_data <- sim_data()
+#' data <- simulated_data
+#' data <- cons_comp_analysis(simulated_data)
 cons_comp_analysis <- function(data,
                                outcome_state = "b_state",
                                pred_trait = c("P", "E"),
@@ -298,7 +162,7 @@ cons_comp_analysis <- function(data,
 
   # max measurement occasions
   out <- split(data, f = data[, id])
-  length_out <- map(out, nrow)
+  length_out <- purrr::map(out, nrow)
   max_o <- max(unlist(length_out))
 
   # function to make all data frames have equal rows
@@ -311,11 +175,11 @@ cons_comp_analysis <- function(data,
     }
   }
   # make all data frames in the list have equal length
-  level_1_id_states <- map(out, ~ make_nrow(., max_o))
+  level_1_id_states <- purrr::map(out, ~ make_nrow(., max_o))
 
   # function to generate the consecutive state variables
   sample_all_o <- function(i){ # rsquare
-    odf <- map(level_1_id_states, ~ .[1:i, ])
+    odf <- purrr::map(level_1_id_states, ~ .[1:i, ])
 
     ## aggregate within person to get the average state of the person
     aggregate_within <- function(odf){
@@ -324,7 +188,7 @@ cons_comp_analysis <- function(data,
                  pre_trait = mean(odf[,pred_state], na.rm = TRUE))
     }
     # aggregate states
-    aggregate_states <- map(odf, aggregate_within)
+    aggregate_states <- purrr::map(odf, aggregate_within)
 
     # combine states in data frame
     aggregate_data <- do.call("rbind", aggregate_states)
@@ -344,11 +208,14 @@ cons_comp_analysis <- function(data,
                          c(id, pred_trait, outcome_state, pred_state))
 
     # do the component analysis
-    apsOut = aps(level2_data, outcome_state, list(pred_trait[1], pred_state,
-                                                  pred_trait[2]))
+    apsOut = yhat::aps(level2_data, outcome_state, list(pred_trait[1],
+                                                        pred_state,
+                                                        pred_trait[2])
+                       )
+
 
     # return parameters
-    commonality <- round(commonality(apsOut)[,2], 3)
+    commonality <- round(yhat::commonality(apsOut)[,2], 3)
     rsquared <- sum(apsOut$APSMatrix[,2])
 
     return(
@@ -359,8 +226,9 @@ cons_comp_analysis <- function(data,
 
 
   # run the above function across all state averages
-  params <- map(1:max_o, sample_all_o)
-  # extract all firt elements of the list
+  params <- purrr::map(1:max_o, sample_all_o)
+
+  # extract all first elements of the list
   c <- lapply(params, "[[", 1)
   rsq <- unlist(lapply(params, "[[", 2))
   params_df <- do.call("rbind", c)
@@ -370,3 +238,146 @@ cons_comp_analysis <- function(data,
          rsq)
   )
 }
+
+
+# Plot Stacked Areas -------------------------------------------------
+
+#' Plot Stacked Areas
+#'
+#' @param data The data frame that needs to be plotted. Should only contain the variables that should appear in the plot.
+#' @param poly.border.col The color of the border of the stacked areas.
+#' @param poly.fill.col The fill of the stacked areas. The areas are colored from top to bottom (i.e. the highest one first).
+#' @param letters The letters that should be placed in the areas. If NULL, the names of the data is used.
+#' @param letter.col The colors of the letters that are plotted.
+#' @param ... Further arguments passed on to the plot function plot(...)
+#'
+#' @return Plots areas that are scaled to 100%.
+#' @export
+#'
+#' @examples
+#' simulated_data <- sim_data()
+#' comp_list <- cons_comp_analysis(simulated_data)
+#' plot_data <- comp_list[[1]]
+#' plot_polygons(plot_data)
+plot_polygons <- function(data,
+                          poly.border.col = "grey10",
+                          poly.fill.col = c("white", "white",
+                                            "white", "white", "#F8CBAE",
+                                            "#FDF2CA", "#9DC3E6"),
+                          letters = c("P,S,E", "S,E", "P,E", "P,S", "E", "S", "P"),
+                          letter.col = "black",
+                          ...){
+
+  is.num <-  unlist(purrr::map(data, is.numeric))
+  nonNumeric <-  names(data)[!is.num]
+  if (any(!is.num)) {
+    stop(paste("The following items/columns are non-numeric. \n ",
+               nonNumeric, collapse=" "), call.=FALSE)
+  }
+
+  xunits <- (nrow(data)) # determine the number of units, because we want to plot from 0 to n.
+  rownames(data) <- 1:xunits
+  # get percentages:
+  total <- rowSums(data, na.rm=TRUE)
+  perc.dat <- as.data.frame(apply(data, 2, function(x) x/total*100))
+  t.perc.dat <- t(perc.dat)
+  # make a vector over which we apply the function that computes the boundaries for the polygons
+  len <- 2:ncol(data)
+  # apply the function in order to get the borders of the polygons
+  res <- rev(lapply(len, function(x) colSums(t.perc.dat[1:x,], na.rm=TRUE)))
+  # add the last polygon to list
+  res[[length(res)+1]] <- t.perc.dat[1,]
+  # compute the lower border ofthe last polygon, i. e. 0, repeated as many times as data has rows
+  lower <- rep(0, xunits) # +1
+
+  # plot the polygons, define the function
+  plot.poly <- function(x){
+    xcoords <-  c(xunits:1, 1:xunits)
+    ycoords <-  c(lower, res[[x]])
+    polygon(xcoords, ycoords, col = poly.fill.col[x], border = poly.border.col)
+  }
+
+  ## plot an empty area
+  plot(NA, ylim=c(1, 110), xlim = c(0, xunits), type="n",
+       yaxt="n", xaxt="n", bty="n", ylab="", xlab="", ...)
+  axis(1)
+
+  # plot the polygons
+  invisible(lapply(1:length(res), function(x) plot.poly(x) ))
+
+  if (ncol(data) != length(poly.fill.col)) {
+    warning("Only ", length(poly.fill.col), " colors were specified, but ", ncol(data), " areas were plotted. Colors were recycled. Please check.")
+  }
+
+  # add letters, default is to names of data
+  if (is.null(letters) ) { letters <- rev(colnames(data)) }
+
+  # add to the result list with the borders another lower border, i. e. the zeros with names
+  names(lower) <- 0:(length(lower)-1)
+  res[[length(res)+1]] <- lower
+
+  # apply the function over the borders, computing the borders and plotting the polygon
+  invisible(
+    lapply(1:(length(res)-1), function(x){
+      y = x + 1
+      # make two empty lists
+      list_low <- list()
+      list_up <- list()
+      # reduce the plotting area left and right
+      list_low[[x]] <- res[[x]][ceiling(length(res[[x]])*.08):floor(length(res[[x]])*.90)]
+      list_up[[x]] <- res[[y]][ceiling(length(res[[y]])*.08):floor(length(res[[y]])*.90)]
+
+      if ( nrow(data) >= 12) {
+        # get the position of the letters, which is where the maximum of the difference between the borders is. if there is more than one maximum, take only the first
+        posx = names(which(list_low[[x]]-list_up[[x]] == max(list_low[[x]]-list_up[[x]])))[1]
+        # compute the mean difference, needs to be in the tighter/reduced lists
+        posy = mean(c(list_low[[x]][posx], list_up[[x]][posx]))
+        text(as.numeric(posx), posy, letters[x], col = letter.col)
+      } else {
+        posx <- (nrow(data)-1)
+        # compute the mean, not in the tight lists (but in 'res', to plot the letter to the right hand side)
+        posy = mean(c(res[[x]][posx], res[[y]][posx]))
+        text(posx-posx*0.03, posy, letters[x], col = letter.col)
+      }
+    })
+  )
+}
+
+
+
+# plot components with explained variance ----------------------------
+
+#' Plot results of components analysis
+#'
+#' @param params_df The results of the function, which is a list with to entries.
+#' @param ... Further arguments passed on to plot_polygons
+#'
+#' @return
+#' Returns a plot with the components
+#' @export
+#'
+#' @examples
+#' data <- sim_data()
+#' comp_list <- cons_comp_analysis(simulated_data)
+#' plot_com(components = comp_list[[1]], rsquared = comp_list[[2]])
+plot_com <- function(components, rsquared, ...){
+  params <- components
+  rsq <- rsquared
+
+  # par (mar = c(5.1, 4.1, 4.1, 0))
+  plot_polygons(params, ...)
+
+  # plot addional explained variance in the plot
+  rsq_plot <- rsq*10+100
+  lines(rsq_plot, col = "black", lwd = 2, lty = 3)
+  min <- round(rsq[1], 2)
+  max <- round(rsq[length(rsq_plot)], 2)
+  text(x = 1, y = 10 + 100, labels=min)
+  text(x = length(rsq_plot), y = 10 + 100, labels=max)
+
+  axis(2, at = c(0, 20, 40, 60, 80, 100), labels = c(0, 20, 40, 60, 80, 100))
+  # add explained variance
+  axis(4, at=c(100, 105, 110), labels = c(0, 50, 100))
+}
+
+
